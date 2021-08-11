44604d44c00285c36f43eac6345ba4ec5240d65d
JasonLuis <jason20luis@gmail.com>
2021-08-11 19:50:38 -0300

    chore: primeiros passos na pagina inicial

--- src/ui/core/components/PortifolioBanner.vue
+++ src/ui/core/components/PortifolioBanner.vue
@@ -1,19 +1,21 @@
 <template>
-  <v-navigation-drawer
-    v-model="drawer"
-    app
-    class="pt-4"
-    color="grey lighten-3"
-    mini-variant
-  >
-    <v-avatar
-      v-for="n in 6"
-      :key="n"
-      :color="`grey ${n === 1 ? 'darken' : 'lighten'}-1`"
-      :size="n === 1 ? 36 : 20"
-      class="d-block text-center mx-auto mb-9"
-    ></v-avatar>
-  </v-navigation-drawer>
+  <v-card class="mx-auto" max-width="344" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">OVERLINE</div>
+        <v-list-item-title class="text-h5 mb-1"> Headline 5 </v-list-item-title>
+        <v-list-item-subtitle
+          >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
+        >
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn outlined rounded text> Button </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
--- src/ui/pages/index.vue
+++ src/ui/pages/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <v-container class="fill-height" fluid>
-    <div class="animate__animated animate__backInUp">
-      <PortifolioMain />
-    </div>
+  <v-container
+    class="fill-height fill-width"
+    style="padding: 0 !important"
+    fluid
+  >
+    <v-row class="fill-height image-background" style="margin: 0" fluid>
+      <v-col cols="12" sm="12">
+        <v-container class="fill-height" fluid>
+          <div class="animate__animated animate__backInUp">
+            <PortifolioMain />
+          </div>
+        </v-container>
+      </v-col>
+    </v-row>
+    <v-row class="fill-height indigo lighten-1" style="margin: 0" fluid>
+      <v-col cols="12" sm="12">
+        <v-container class="fill-height" fluid> Continua... </v-container>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+
+<style scoped>
+.image-background {
+  background-image: url('~/ui/core/assets/background3.svg');
+}
+</style>
